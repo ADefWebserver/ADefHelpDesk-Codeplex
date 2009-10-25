0c4914087f97975b6c2dc122a3450239adf64553
adefwebserver_cp <SND\adefwebserver_cp>
2009-10-25 22:12:48 +0000

    ADefHelpDeskUpgradeAdvisor DotNetNuke Module (web service)

--- DotNetNuke/App_Code/ADefHelpDeskUpgradeAdvisor/Webservice.cs
+++ DotNetNuke/App_Code/ADefHelpDeskUpgradeAdvisor/Webservice.cs
@@ -33,6 +33,9 @@ using System.IdentityModel;
 using System.Collections.Generic;
 using System.Linq;
 using DotNetNuke.Entities.Users;
+using System.Reflection;
+using System.Web.Compilation;
+using DotNetNuke.Entities.Modules;
 
 namespace ADefWebserver.Modules.ADefHelpDeskUpgradeAdvisor
 {
@@ -60,14 +63,150 @@ namespace ADefWebserver.Modules.ADefHelpDeskUpgradeAdvisor
             if (Authendication.IsUserValid())
             {
                 UserInfo objUser = Authendication.GetUserInfo();
-                response = String.Format("Hello {0}! [{1}]", objUser.DisplayName, strIPAddress);
-            }
-            else
-            {
-                response = String.Format("Hello [{0}]", strIPAddress);
+                response = String.Format("Hello {0}!", objUser.DisplayName);
             }
 
             return response;
+        }
+        #endregion
+
+        #region CurrentVersionOfADefHelpDeskUpgradeAdvisor
+        [WebMethod]
+        public string CurrentVersionOfADefHelpDeskUpgradeAdvisor(int PortalID, int ModuleId, int UserID, string Password)
+        {
+            string strIPAddress = this.Context.Request.UserHostAddress;
+            ADefHelpDeskAuthendicationHeader SilverlightDesktopAuthendicationHeader = new ADefHelpDeskAuthendicationHeader();
+            SilverlightDesktopAuthendicationHeader.PortalID = PortalID;
+            SilverlightDesktopAuthendicationHeader.UserID = UserID;
+            SilverlightDesktopAuthendicationHeader.Password = Password;
+            SilverlightDesktopAuthendicationHeader.ModuleId = ModuleId;
+            SilverlightDesktopAuthendicationHeader.IPAddress = strIPAddress;
+
+            string response = "";
+
+            Authendication Authendication = new Authendication(SilverlightDesktopAuthendicationHeader);
+            if (Authendication.IsUserValid())
+            {
+               ADefHelpDeskWebProxy.ADefHelpDeskProxy.WebService objWebServiceSoapClient = new ADefHelpDeskWebProxy.ADefHelpDeskProxy.WebService();
+
+               response = objWebServiceSoapClient.CurrentUpgradeVersion();
+            }
+
+            return response;
+        }
+        #endregion
+
+        #region GetUpgradeItems
+        [WebMethod]
+        public List<UpgradeItems> GetUpgradeItems(int PortalID, int ModuleId, int UserID, string Password, string strCurrentVersion)
+        {
+            string strIPAddress = this.Context.Request.UserHostAddress;
+            ADefHelpDeskAuthendicationHeader SilverlightDesktopAuthendicationHeader = new ADefHelpDeskAuthendicationHeader();
+            SilverlightDesktopAuthendicationHeader.PortalID = PortalID;
+            SilverlightDesktopAuthendicationHeader.UserID = UserID;
+            SilverlightDesktopAuthendicationHeader.Password = Password;
+            SilverlightDesktopAuthendicationHeader.ModuleId = ModuleId;
+            SilverlightDesktopAuthendicationHeader.IPAddress = strIPAddress;
+
+            List<UpgradeItems> ColUpgradeItems = new List<UpgradeItems>();
+
+            Authendication Authendication = new Authendication(SilverlightDesktopAuthendicationHeader);
+            if (Authendication.IsUserValid())
+            {
+                ADefHelpDeskWebProxy.ADefHelpDeskProxy.WebService objWebServiceSoapClient = new ADefHelpDeskWebProxy.ADefHelpDeskProxy.WebService();
+
+                var tmpUpgradeItems = objWebServiceSoapClient.UpgradeCheck(strCurrentVersion);
+
+                foreach (var item in tmpUpgradeItems)
+                {
+                    ColUpgradeItems.Add
+                        (new UpgradeItems
+                        {
+                            Item = item.Item,
+                            ItemDescription = item.ItemDescription,
+                            ItemDetails = item.ItemDetails,
+                            ItemMethodName = item.ItemMethodName
+                        });
+                }
+            }
+
+            return ColUpgradeItems;
+        }
+        #endregion
+
+        #region CurrentVersionOfADefHelpDesk
+        [WebMethod]
+        public string CurrentVersionOfADefHelpDesk(int PortalID, int ModuleId, int UserID, string Password)
+        {
+            string strIPAddress = this.Context.Request.UserHostAddress;
+            ADefHelpDeskAuthendicationHeader SilverlightDesktopAuthendicationHeader = new ADefHelpDeskAuthendicationHeader();
+            SilverlightDesktopAuthendicationHeader.PortalID = PortalID;
+            SilverlightDesktopAuthendicationHeader.UserID = UserID;
+            SilverlightDesktopAuthendicationHeader.Password = Password;
+            SilverlightDesktopAuthendicationHeader.ModuleId = ModuleId;
+            SilverlightDesktopAuthendicationHeader.IPAddress = strIPAddress;
+
+            string response = "";
+
+            Authendication Authendication = new Authendication(SilverlightDesktopAuthendicationHeader);
+            if (Authendication.IsUserValid())
+            {
+                ModuleController objModuleController = new ModuleController();
+                var colModuleInfo = objModuleController.GetAllModules().Cast<ModuleInfo>().ToList();
+
+                var objModule = (from Module in colModuleInfo
+                                 where Module.ModuleTitle == "ADefHelpDesk"
+                                 select Module).FirstOrDefault();
+
+                if (objModule != null)
+                {
+                    response = objModule.Version;
+                }
+            }
+
+            return response;
+        }
+        #endregion
+
+        #region ADefHelpDeskLastSearchCurrentPagePresent
+        [WebMethod]
+        public bool ADefHelpDeskLastSearchCurrentPagePresent(int PortalID, int ModuleId, int UserID, string Password)
+        {
+            string strIPAddress = this.Context.Request.UserHostAddress;
+            ADefHelpDeskAuthendicationHeader SilverlightDesktopAuthendicationHeader = new ADefHelpDeskAuthendicationHeader();
+            SilverlightDesktopAuthendicationHeader.PortalID = PortalID;
+            SilverlightDesktopAuthendicationHeader.UserID = UserID;
+            SilverlightDesktopAuthendicationHeader.Password = Password;
+            SilverlightDesktopAuthendicationHeader.ModuleId = ModuleId;
+            SilverlightDesktopAuthendicationHeader.IPAddress = strIPAddress;
+
+            bool response = false;
+
+            Authendication Authendication = new Authendication(SilverlightDesktopAuthendicationHeader);
+            if (Authendication.IsUserValid())
+            {
+                var TheClass = BuildManager.GetType("ADefWebserver.Modules.ADefHelpDesk.ADefHelpDeskDALDataContext", true, true);
+                var TheTable = TheClass.Module.GetTypes().Where(x => x.Name == "ADefHelpDesk_LastSearch");
+                var TheField = TheTable.FirstOrDefault().GetProperties().Where(y => y.Name.Contains("CurrentPage"));
+
+                if (TheField.FirstOrDefault() != null)
+                {
+                    response = true;
+                }
+            }
+
+            return response;
+        }
+        #endregion
+
+        #region UpgradeItems
+        [Serializable]
+        public class UpgradeItems
+        {
+            public string Item { get; set; }
+            public string ItemDescription { get; set; }
+            public string ItemDetails { get; set; }
+            public string ItemMethodName { get; set; }
         }
         #endregion
     }
